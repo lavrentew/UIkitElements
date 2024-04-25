1f48fdf45003a496e7a0df4f904a038c403e89aa
lavrentew <n89258101522@gmail.com>
2024-04-25 15:09:39 +0300

    Initial Commit

---
+++ UIkitElements/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIkitElements
+//
+//  Created by Николай Лаврентьев on 4/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
